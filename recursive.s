3f8775bb6da7ac01b12d54c62f0b8afce37b827b
Naz <nazarethisaiah@hotmail.com>
2020-03-31 22:55:35 -0400

    comments in file

--- recursive.s
+++ recursive.s
@@ -6,3 +6,25 @@
   main:
      #taking the user input for hexadecimal numbers
       li $v0, 8
+      la $a0,  max_input
+      li $a1, 1001
+      syscall 
+
+     #loading the input to a  register for conversion to decimal 
+      la $s0, max_input 
+      li $s1, 0     #start pointer
+      li $s2, 0     #end pointer
+
+     word_list:
+       la $s1, ($s2)
+
+     substring: 
+       add $t1, $s0, $s2 	#iterator 
+       lb $t2, 0($t1) 		#loading the current character 
+       
+       # a few criteron to exit the loop while iterating the substrings 
+       beq $t2, 0, end_substring
+       beq $t2, 10, end_substring
+       beq $t2, 44, end_substring
+
+
